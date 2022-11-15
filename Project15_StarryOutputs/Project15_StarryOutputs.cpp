021ecdd935527b112f6819ab68ad00e089f3abb4
Josh-Mullane <114984722+Josh-Mullane@users.noreply.github.com>
2022-11-15 09:40:28 +0000

    Update Project15_StarryOutputs.cpp

--- Project15_StarryOutputs/Project15_StarryOutputs.cpp
+++ Project15_StarryOutputs/Project15_StarryOutputs.cpp
@@ -5,12 +5,11 @@ int main()
 {
     bool goAgain = false;
     int lineNum;
-    string choice;
-    string stars;
+    char choice;
+    string stars = "   ";
 
     while (!goAgain)
     {
-        stars = "*  ";
         cout << "Enter a line number between 1 and 10" << endl;
         cin >> lineNum;
         for (int i = 0; i < lineNum; i++)
@@ -19,18 +18,17 @@ int main()
                 cout << stars << endl;
             }
         cout << "Would you like to go again? Y/N" << endl;
-        cin; choice;
-        if (choice == "n" || choice == "N")
+        cin >> choice;
+
+        if (choice == 'n' || choice == 'N')
         {
             goAgain = true;
         }
         else
         {
+            stars = "   ";
             goAgain = false;
         }
-        
-
-      
     }
 }
 
